951eb6ed34611b6fce8dc81a3c63c59119fa5c32
golnazo <golnaz.onsori@gmail.com>
2024-07-05 23:23:56 +0330

    changes

---
+++ bump-version.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "No version bump type provided. Please provide 'patch', 'minor', or 'major'."
+  exit 1
+fi
+
+VERSION_BUMP_TYPE=$1
+
+CURRENT_VERSION=$(node -p "require('./package.json').version")
+TAG_EXISTS=$(git tag -l "v$CURRENT_VERSION")
+
+if [ -z "$TAG_EXISTS" ]; then
+  echo "Bumping version with type: $VERSION_BUMP_TYPE"
+  npm version $VERSION_BUMP_TYPE
+else
+  echo "Tag v$CURRENT_VERSION already exists. Skipping version bump."
+fi
