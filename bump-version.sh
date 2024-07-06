3431825e4dc9733655ee8095926aef4e12912e41
golnazo <golnaz.onsori@gmail.com>
2024-07-06 10:52:39 +0330

    changes

--- bump-version.sh
+++ bump-version.sh
@@ -12,10 +12,13 @@ VERSION_BUMP_TYPE=$1
 CURRENT_VERSION=$(node -p "require('./package.json').version")
 TAG_EXISTS=$(git tag -l "v$CURRENT_VERSION")
 
-if [ -z "$TAG_EXISTS" ]; then
+# if [ -z "$TAG_EXISTS" ]; then
   echo "Bumping version with type: $VERSION_BUMP_TYPE"
   npm version $VERSION_BUMP_TYPE
-
+  
+# else
+#   echo "Tag v$CURRENT_VERSION already exists. Bumping to the next available version."
+#   npm version patch  # Example: Always bumping to the next patch version
 fi
 
 # if [ -z "$1" ]; then
