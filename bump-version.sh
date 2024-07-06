da8ef99e7cc54ec08e7264bcddcb94d6ea090a7f
golnazo <golnaz.onsori@gmail.com>
2024-07-06 11:04:31 +0330

    changes

--- bump-version.sh
+++ bump-version.sh
@@ -8,16 +8,18 @@ fi
 VERSION_BUMP_TYPE=$1
 
 CURRENT_VERSION=$(node -p "require('./package.json').version")
-TAG_EXISTS=$(git tag -l "v$CURRENT_VERSION")
+NEXT_VERSION=$(npm --no-git-tag-version version $VERSION_BUMP_TYPE --json | jq -r '.version')
+TAG_EXISTS=$(git tag -l "v$NEXT_VERSION")
 
 if [ -z "$TAG_EXISTS" ]; then
-  echo "Bumping version with type: $VERSION_BUMP_TYPE"
+  echo "Bumping version from $CURRENT_VERSION to $NEXT_VERSION"
   npm version $VERSION_BUMP_TYPE --no-git-tag-version
-  NEW_VERSION=$(node -p "require('./package.json').version")
   git add package.json
-  git commit -m "Bump version to $NEW_VERSION"
-  git tag "v$NEW_VERSION"
+  git commit -m "Bump version to $NEXT_VERSION"
+  git tag "v$NEXT_VERSION"
   git push origin master --tags
 else
-  echo "Tag v$CURRENT_VERSION already exists. Skipping version bump."
+  echo "Tag v$NEXT_VERSION already exists. Skipping version bump."
+  # Revert the version change in package.json
+  npm version $CURRENT_VERSION --no-git-tag-version --allow-same-version
 fi
