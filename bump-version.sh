01e93b72268319dd787e58594224f5a770006446
golnazo <golnaz.onsori@gmail.com>
2024-07-06 10:34:01 +0330

    changes

--- bump-version.sh
+++ bump-version.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#!/bin/sh
 
 if [ -z "$1" ]; then
   echo "No version bump type provided. Please provide 'patch', 'minor', or 'major' as an argument."
@@ -7,16 +8,30 @@ fi
 
 VERSION_BUMP_TYPE=$1
 
-CURRENT_VERSION=$(node -p "require('./package.json').version")
-TAG_EXISTS=$(git tag -l "v$CURRENT_VERSION")
+# Bump the version and handle errors
+npm version $VERSION_BUMP_TYPE || exit 1
 
-if [ -z "$TAG_EXISTS" ]; then
-  echo "Bumping version with type: $VERSION_BUMP_TYPE"
-  npm version $VERSION_BUMP_TYPE
-else
-  echo "Tag v$CURRENT_VERSION already exists. Bumping to the next available version."
-  npm version patch  # Example: Always bumping to the next patch version
-fi
+# Push the changes and tags
+git push --follow-tags origin master || exit 1
+
+
+# if [ -z "$1" ]; then
+#   echo "No version bump type provided. Please provide 'patch', 'minor', or 'major' as an argument."
+#   exit 1
+# fi
+
+# VERSION_BUMP_TYPE=$1
+
+# CURRENT_VERSION=$(node -p "require('./package.json').version")
+# TAG_EXISTS=$(git tag -l "v$CURRENT_VERSION")
+
+# if [ -z "$TAG_EXISTS" ]; then
+#   echo "Bumping version with type: $VERSION_BUMP_TYPE"
+#   npm version $VERSION_BUMP_TYPE
+# else
+#   echo "Tag v$CURRENT_VERSION already exists. Bumping to the next available version."
+#   npm version patch  # Example: Always bumping to the next patch version
+# fi
 
 # if [ -z "$1" ]; then
 #   echo "No version bump type provided. Please provide 'patch', 'minor', or 'major' as an argument."
