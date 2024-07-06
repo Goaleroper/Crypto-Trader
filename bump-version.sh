48d206c8872a247db9efc57ad2f96dde8f204f0c
golnazo <golnaz.onsori@gmail.com>
2024-07-06 10:56:45 +0330

    changes

--- bump-version.sh
+++ bump-version.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-  echo "No version bump type provided. Using 'minor' as default."
-  VERSION_BUMP_TYPE="patch"
-else
-  VERSION_BUMP_TYPE=$1
+  echo "No version bump type provided. Please provide 'patch', 'minor', or 'major' as an argument."
+  exit 1
 fi
 
 VERSION_BUMP_TYPE=$1
@@ -12,30 +10,14 @@ VERSION_BUMP_TYPE=$1
 CURRENT_VERSION=$(node -p "require('./package.json').version")
 TAG_EXISTS=$(git tag -l "v$CURRENT_VERSION")
 
-# if [ -z "$TAG_EXISTS" ]; then
+if [ -z "$TAG_EXISTS" ]; then
   echo "Bumping version with type: $VERSION_BUMP_TYPE"
-  npm version $VERSION_BUMP_TYPE
-  
-# else
-#   echo "Tag v$CURRENT_VERSION already exists. Bumping to the next available version."
-#   npm version patch  # Example: Always bumping to the next patch version
-# fi
-
-# if [ -z "$1" ]; then
-#   echo "No version bump type provided. Please provide 'patch', 'minor', or 'major' as an argument."
-#   exit 1
-# fi
-
-# VERSION_BUMP_TYPE=$1
-
-# # Bump the version and check the new version
-# NEW_VERSION=$(npm version $VERSION_BUMP_TYPE)
-# TAG_EXISTS=$(git tag -l "v$NEW_VERSION")
-
-# if [ -z "$TAG_EXISTS" ]; then
-#   echo "Bumping version to $NEW_VERSION"
-# else
-#   echo "Tag v$NEW_VERSION already exists. Skipping version bump."
-#   exit 0
-# fi
-
+  npm version $VERSION_BUMP_TYPE --no-git-tag-version
+  NEW_VERSION=$(node -p "require('./package.json').version")
+  git add package.json
+  git commit -m "Bump version to $NEW_VERSION"
+  git tag "v$NEW_VERSION"
+  git push origin master --tags
+else
+  echo "Tag v$CURRENT_VERSION already exists. Skipping version bump."
+fi
