33d546945d3cc1079a7c4acb407ecdeb04a334fb
golnazo <golnaz.onsori@gmail.com>
2024-07-06 11:56:58 +0330

    changes

--- bump-version.sh
+++ bump-version.sh
@@ -13,7 +13,7 @@ VERSION_BUMP_TYPE=$1
 # if [ -z "$TAG_EXISTS" ]; then
   echo "Bumping version with type: $VERSION_BUMP_TYPE"
   npm version $VERSION_BUMP_TYPE
-  git push origin master --tags
+  git push --follow-tags origin master
 # else
 #   echo "Tag v$CURRENT_VERSION already exists. Bumping to the next available version."
 #   npm version patch  # Example: Always bumping to the next patch version
